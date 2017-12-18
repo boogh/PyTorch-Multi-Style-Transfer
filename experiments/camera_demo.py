1c9f3997da808b4db87d45bb42c660f3b933d8d2
Experience Center <experiencecenter@49064464-002.local>
2017-12-18 14:26:21 +0100

    second versiohn

--- experiments/camera_demo.py
+++ experiments/camera_demo.py
@@ -10,6 +10,14 @@ import utils
 from utils import StyleLoader
 
 def run_demo(args, mirror=False):
+	#code to get focus in window
+	cv2.namedWindow("GetFocus", cv2.WINDOW_NORMAL)
+	img = np.zeros((100, 100, 1), dtype = "uint8")
+	cv2.imshow("GetFocus", img);
+	cv2.setWindowProperty("GetFocus", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+	cv2.setWindowProperty("GetFocus", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+	cv2.destroyWindow("GetFocus")
+	#load style model
 	style_model = Net(ngf=args.ngf)
 	style_model.load_state_dict(torch.load(args.model))
 	style_model.eval()
@@ -32,22 +40,28 @@ def run_demo(args, mirror=False):
 	cam.set(4, height)
 	key = 0
 	idx = 0
+	#start unfrozen
 	freeze2art = False 
-	while True:
+	#load styles on first use only - speeds up
+	style_loaded =False
+	#stop when q pressed
+	stopped = False
+	while not stopped:
 		# read frame
-		idx += 1
 		if not freeze2art:
 			ret_val, img = cam.read()
 			if mirror: 
 				img = cv2.flip(img, 1)
 		cimg = img.copy()
+		# if the f has been pressed
 		if freeze2art:
 			img = np.array(img).transpose(2, 0, 1)
-		# changing style 
-		#if idx%20 == 1:
-			style_v = style_loader.get(int(idx/20))
-			style_v = Variable(style_v.data, volatile=True)
-			style_model.setTarget(style_v)
+			# changing style (n or p has been pressed or first run)
+			if not style_loaded:
+				style_v = style_loader.get(idx)
+				style_v = Variable(style_v.data, volatile=True)
+				style_model.setTarget(style_v)
+				style_loaded = True
 
 			img=torch.from_numpy(img).unsqueeze(0).float()
 			if args.cuda:
@@ -65,25 +79,63 @@ def run_demo(args, mirror=False):
 			img = img.transpose(1, 2, 0).astype('uint8')
 			simg = simg.transpose(1, 2, 0).astype('uint8')
 
-		# display
+			# display
+			#   resize the used painting
 			simg = cv2.resize(simg,(swidth, sheight), interpolation = cv2.INTER_CUBIC)
+			#   include in the left image
 			cimg[0:sheight,0:swidth,:]=simg
-			img = np.concatenate((cimg,img),axis=1)
-		cv2.imshow('MSG Demo', img)
+			#   create merge of 2 images
+			dimg = np.concatenate((cimg,img),axis=1)
+		else:
+			# load on style change or first use
+			if not style_loaded:
+				style_v = style_loader.get(idx)
+				style_v = Variable(style_v.data, volatile=True)
+				style_model.setTarget(style_v)
+				style_loaded = True
+			if args.cuda:
+				simg = style_v.cpu().data[0].numpy()
+			else:
+				simg = style_v.data[0].numpy()
+			simg = simg.transpose(1, 2, 0).astype('uint8')
+
+			# display
+			#   resize the used painting
+			simg = cv2.resize(simg,(swidth, sheight), interpolation = cv2.INTER_CUBIC)
+			dimg = img.copy()
+			#   include in the image
+			dimg[0:sheight,0:swidth,:]=simg
+		# put the image in the window
+		cv2.imshow('MSG Demo', dimg)
 		if freeze2art:
-			while True:
+			resumed = False
+			#wait for r (resume) or q (quit)
+			while not resumed:
 				key = cv2.waitKey(1)
 				if key == ord('r'):
 					freeze2art = False
-					break
-		##cv2.imwrite('stylized/%i.jpg'%idx,img)
+					resumed = True
+				if key == ord('q'):
+					stopped = True
+					resumed = True
+		# wait for keys
 		key = cv2.waitKey(1)
-		#if args.record:
-		#	out.write(img)
 		if key == 27: 
-			break
+			stopped = True
+		if key == ord('q'):
+			stopped = True
 		if key == ord('f'):
 			freeze2art = True
+		if key == ord('n'):
+			idx+=1
+			if idx>8:
+				idx=0
+			style_loaded = False
+		if key == ord('p'):
+			idx-=1
+			if idx<0:
+				idx=8
+			style_loaded = False
 	cam.release()
 	if args.record:
 		out.release()
